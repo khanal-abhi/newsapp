520b3f9303d3bff2da89c5ea48dbb425afcb5347
Abhi Khanal <khanal.abhi@gmail.com>
2017-02-17 18:45:22 -0500

    Initialize repository.

---
+++ app/src/main/java/abhinash/io/newsfeed/service/FecthNewsFeed.java
@@ -0,0 +1,10 @@
+package abhinash.io.newsfeed.service;
+
+/**
+ * Created by khanal on 2/17/17.
+ * This service will be used to fecth data from the api.
+ */
+
+public class FecthNewsFeed {
+
+}
---
+++ app/src/main/java/abhinash/io/newsfeed/util/AppConstants.java
@@ -0,0 +1,13 @@
+package abhinash.io.newsfeed.util;
+
+/**
+ * Created by khanal on 2/17/17.
+ * This holds the costants required for the app.
+ */
+
+public interface AppConstants {
+
+    String API_KEY_KEY = "api-key";
+    String API_KEY = "";
+    int pageSize = 10;
+}
